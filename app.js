59f4942a46d76fcaa5f9dc07ded654e8abd6aeac
Anshul Verma <anshulverma96@gmail.com>
2018-02-24 09:25:11 +0530

    ansh

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var compression = require('compression');
 const model=require('./api/model.js');
 /* var port = process.env.PORT||8080;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/shreeshyamroadways'; */
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://ds147118.mlab.com:47118/heroku_dn67chxs';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://heroku_dn67chxs:root@ds147118.mlab.com:47118/heroku_dn67chxs';
 const db = require('./api/dbconfig/db');
 const http_IP = process.env.IP || '192.168.1.8';
 
